dc88af1afc2d6e4a3a5ac51060a34a2feff10f1b
fotini <f.batzoni@gmail.com>
2023-03-18 18:25:42 +0200

    Setting an HTTP request

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { IProduct } from "./product";
 
 @Injectable({
@@ -6,29 +8,11 @@ import { IProduct } from "./product";
 })
 
 export class ProductService {
+    private productUrl = 'api/products/products.json';
 
-    getProducts() : IProduct[] {
-        return [
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate":"March 18, 2021",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating" : 4.2,
-                "imageUrl":"assets/images/garden_cart.png"
-            },
-            {
-                "productId": 5,
-                "productName": "Hammer",
-                "productCode": "TBX-0048",
-                "releaseDate":"May 21, 2021",
-                "description": "Curved claw steel hammer",
-                "price": 8.9,
-                "starRating" : 4.8,
-                "imageUrl":"assets/images/hammer.png"
-            }
-        ]
+    constructor(private http: HttpClient) {}
+
+    getProducts() : Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl);
     }
 }
